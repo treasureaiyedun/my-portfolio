e08fd8925531beda2486819f5f5ca8941d027252
Treasure Aiyedun <treasureisatechsis@gmail.com>
2024-08-25 18:30:48 +0100

    modified the index.html and config file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/, 
+    exclude: /node_modules/, // Exclude node_modules
   },
-})
+});
